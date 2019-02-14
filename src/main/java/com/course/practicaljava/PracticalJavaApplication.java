4f679c6442e06723266ce34eae34db54318a3deb
Jonathan Youyoutte <youyoutte.jonathan@gmail.com>
2019-02-14 16:49:05 +0100

    Initial commit

---
+++ src/main/java/com/course/practicaljava/PracticalJavaApplication.java
@@ -0,0 +1,19 @@
+package com.course.practicaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PracticalJavaApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return createSpringApplicationBuilder().sources(PracticalJavaApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalJavaApplication.class, args);
+	}
+}
