55c921330f3a78c9b4dab740dac8b47bde016778
Jonathan Youyoutte <jonathan.youyoutte@laposte.net>
2019-02-15 16:16:33 +0100

    Commit Car.java
    Add getters and setters for attributes

--- src/main/java/com/course/practicaljava/domain/Car.java
+++ src/main/java/com/course/practicaljava/domain/Car.java
@@ -1,13 +1,30 @@
 package com.course.practicaljava.domain;
 
+import java.util.Date;
+
 public class Car {
 	private String brand, color, type;
+	private int price;
+	private boolean available;
+	private Date firstReleaseDate;
+	
+	public Car(){}
 
 	public Car(String brand, String color, String type) {
 		super();
 		this.brand = brand;
 		this.color = color;
 		this.type = type;
+	}
+
+	public Car(String brand, String color, String type, int price, boolean available, Date firstReleaseDate) {
+		super();
+		this.brand = brand;
+		this.color = color;
+		this.type = type;
+		this.price = price;
+		this.available = available;
+		this.firstReleaseDate = firstReleaseDate;
 	}
 
 	public String getBrand() {
@@ -18,8 +35,24 @@ public class Car {
 		return color;
 	}
 
+	public Date getfirstReleaseDate() {
+		return firstReleaseDate;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
 	public String getType() {
 		return type;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
 	}
 
 	public void setBrand(String brand) {
@@ -30,12 +63,23 @@ public class Car {
 		this.color = color;
 	}
 
+	public void setfirstReleaseDate(Date firstReleaseDate) {
+		this.firstReleaseDate = firstReleaseDate;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
 
 	@Override
 	public String toString() {
-		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + "]";
+		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + ", price=" + price + ", available="
+				+ available + ", firstReleaseDate=" + firstReleaseDate + "]";
 	}
+
+	
 }
--- src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
+++ src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
@@ -1,8 +1,12 @@
 package com.course.practicaljava.rest.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,8 @@ import com.course.practicaljava.service.CarService;
 @RestController
 @RequestMapping("/api/car/v1")
 public class CarRestController {
+	
+		private Logger log = LoggerFactory.getLogger(CarRestController.class);
 
 		@Autowired
 		private CarService carService;
@@ -20,5 +26,11 @@ public class CarRestController {
 		@GetMapping(path="/random", produces = MediaType.APPLICATION_JSON_VALUE)
 		public Car random() {
 			return carService.generateCar();
+		}
+		
+		@PostMapping(path ="/echo", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
+		public String echo (@RequestBody Car car) {
+			log.info("The car is : " + car);		
+			return car.toString();			
 		}
 }
